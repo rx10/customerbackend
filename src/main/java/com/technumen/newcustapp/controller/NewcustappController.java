40b43e2947785af5d875a630995d7f2ce9c7e14c
rx <76666903+rx10@users.noreply.github.com>
2025-08-10 03:19:21 +0530

    change cors domain

--- src/main/java/com/technumen/newcustapp/controller/NewcustappController.java
+++ src/main/java/com/technumen/newcustapp/controller/NewcustappController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://apifrontend.up.railway.app/")
 @RestController
 public class NewcustappController {
     private final CustRepository repository;
